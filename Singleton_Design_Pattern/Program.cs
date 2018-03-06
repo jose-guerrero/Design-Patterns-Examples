d4a810369ab9cad0322b03186a6a0e1a5210ea4b
jose-guerrero <jose.guerrerog@outlook.com>
2018-03-06 19:00:33 +1100

    Singleton Example

---
+++ Singleton_Design_Pattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Design_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton fromEmployee = Singleton.GetInstance;
+            fromEmployee.PrintDetails("From Employee");
+            
+            Singleton s2 = Singleton.GetInstance;
+            fromEmployee.PrintDetails("From Student");
+        }
+    }
+}
