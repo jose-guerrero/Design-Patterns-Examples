9f5835677abe1a8239b689a2a69e247ed67237ee
jose-guerrero <jose.guerrerog@outlook.com>
2018-03-14 17:14:47 +1100

    try-catch basic example

---
+++ ExceptionTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ExceptionTest
+{
+    class Program
+    {
+
+        static double SafeDivision(double x, double y)
+        {
+            if (y == 0)
+                throw new System.DivideByZeroException();
+            return x / y;
+        }
+        static void Main(string[] args)
+        {
+            double a = 98, b = 0;
+            double result = 0;
+
+            try
+            {
+                result = SafeDivision(a, b);
+                Console.WriteLine("{0} divided by {1} = {2}", a, b, result);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Attempted divide by zero.");
+            }
+        }
+    }
+}
